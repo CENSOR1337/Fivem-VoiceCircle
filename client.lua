b2e7ad21d8554357f9dd26c8d1fa825598d08bdb
CENSOR_PC\CENSOR <meteetanathip@gmail.com>
2022-07-21 05:02:09 +0700

    perf: using local function to perfrom

--- client.lua
+++ client.lua
@@ -8,6 +8,10 @@ local proximityRange = 0.0
 local lerpRange = 0.0
 local drawDuration = 5000
 
+local _DrawMarker = DrawMarker
+local _math_floor = math.floor
+local _getEntityCoords = GetEntityCoords
+
 local function Lerp(a, b, t)
     return a + (b - a) * t
 end
@@ -15,11 +19,11 @@ end
 local function CreateCricleThread()
     CreateThread(function()
         while GetGameTimer() < endTime do
-            local localPedCoords = GetEntityCoords(PlayerPedId())
+            local localPedCoords = _getEntityCoords(PlayerPedId())
             localPedCoords = vector3(localPedCoords.x, localPedCoords.y, localPedCoords.z - 0.1)
-            local drawAlpha = math.floor((endTime - GetGameTimer()) / drawDuration * 255)
-            DrawMarker(1, localPedCoords, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, lerpRange, lerpRange, 0.125, currentVoiceMode.color.r, currentVoiceMode.color.g, currentVoiceMode.color.b, drawAlpha, false, true, 2, nil, nil, false)
-            DrawMarker(1, localPedCoords, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, lerpRange, lerpRange, -0.125, currentVoiceMode.color.r, currentVoiceMode.color.g, currentVoiceMode.color.b, drawAlpha, false, true, 2, nil, nil, false)
+            local drawAlpha = _math_floor((endTime - GetGameTimer()) / drawDuration * 255)
+            _DrawMarker(1, localPedCoords, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, lerpRange, lerpRange, 0.125, currentVoiceMode.color.r, currentVoiceMode.color.g, currentVoiceMode.color.b, drawAlpha, false, true, 2, nil, nil, false)
+            _DrawMarker(1, localPedCoords, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, lerpRange, lerpRange, -0.125, currentVoiceMode.color.r, currentVoiceMode.color.g, currentVoiceMode.color.b, drawAlpha, false, true, 2, nil, nil, false)
             lerpRange = Lerp(lerpRange, proximityRange, lerpSpeed)
             Wait(0)
         end
